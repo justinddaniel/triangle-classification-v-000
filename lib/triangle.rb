650b35fdadcb1d2c8f043d9fd8a3cbc5081709ef
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 19:58:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,7 @@
 class Triangle
 
+  attr_accessor :length1, :length2, :length3
+
   def initialize(length1, length2, length3)
     @length1 = length1
     @length2 = length2
@@ -7,23 +9,28 @@ class Triangle
   end
 
   def kind
-    if self.length1 + self.length2 > self.length3 && 
+    if self.length1 + self.length2 > self.length3 &&
       self.length1 + self.length3 > self.length2 &&
       self.length2 + self.length3 > self.length1
       if self.length1 == self.length2 == self.length3
         "equilateral"
       elsif self.length1 != self.length2 =! self.length3
         "scalene"
-      else 
+      else
         "isosceles"
       end
-        
     else
       begin
         raise TriangleError
       rescue TriangleError => error
           puts error.message
       end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle does not have valid lengths"
     end
   end
 
