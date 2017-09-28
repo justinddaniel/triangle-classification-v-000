518f602c3b7d159c388998c561cf2cf7496a5209
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 20:04:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,10 @@ class Triangle
   def kind
     if self.length1 + self.length2 > self.length3 &&
       self.length1 + self.length3 > self.length2 &&
-      self.length2 + self.length3 > self.length1
+      self.length2 + self.length3 > self.length1 &&
+      self.length1 > 0 &&
+      self.length2 > 0 &&
+      self.length3 > 0
       if self.length1 == self.length2 &&
         self.length2 == self.length3
         :equilateral
